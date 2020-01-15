ffc27eb69049e67d30a4adc48945884477051ced
James Hu <hello@james.hu>
2020-01-14 23:38:28 -0800

    Set default browser to Firefox in finicky and allow option+click for Chrome

--- home/.finicky.js
+++ home/.finicky.js
@@ -1,9 +1,16 @@
 module.exports = {
-  defaultBrowser: 'Google Chrome',
+  defaultBrowser: 'Firefox',
   handlers: [
+    // Open all Spotify links in Spotify
     {
       match: finicky.matchDomains('open.spotify.com'),
       browser: 'Spotify',
+    },
+
+    // Open links in Google Chrome when the option key is pressed
+    {
+      match: ({ keys }) => keys.option,
+      browser: 'Google Chrome',
     },
   ],
 }
